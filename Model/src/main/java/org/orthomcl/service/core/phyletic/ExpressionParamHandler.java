a9b94557e870a844978fadfd0627933a41fca183
Richard Demko <rdemko@palm.penn.apidb.org>
2025-04-17 16:54:48 -0400

    Updating Expression Param Handler for phyletic pattern

--- Model/src/main/java/org/orthomcl/service/core/phyletic/ExpressionParamHandler.java
+++ Model/src/main/java/org/orthomcl/service/core/phyletic/ExpressionParamHandler.java
@@ -12,84 +12,89 @@ import org.gusdb.wdk.model.query.spec.QueryInstanceSpec;
 
 public class ExpressionParamHandler extends StringParamHandler {
 
-  private static final Logger LOG = Logger.getLogger(ExpressionParamHandler.class);
+    private static final Logger LOG = Logger.getLogger(ExpressionParamHandler.class);
 
-  private static final String GROUP_SQL =  "(SELECT group_id FROM ( " + 
-   "SELECT group_id, " + "SUM(CAST(number_of_taxa AS INTEGER)) AS number_of_taxa, " + "SUM(CAST(number_of_proteins AS INTEGER)) AS number_of_proteins " + 
-   "FROM apidb.orthologgrouptaxon " + "WHERE lower(three_letter_abbrev) IN (";
+    // PostgreSQL-compatible SQL (replacing MINUS with EXCEPT)
+  private static final String GROUP_SQL = "(SELECT group_id FROM ( " +
+    "SELECT group_id, " +
+    "SUM(CAST(number_of_taxa AS INTEGER)) AS number_of_taxa, " +
+    "SUM(CAST(number_of_proteins AS INTEGER)) AS number_of_proteins " +
+    "FROM apidb.orthologgrouptaxon " +
+      "WHERE lower(three_letter_abbrev) IN (";
 
-  private final ExpressionParser _parser = new ExpressionParser();
+    private final ExpressionParser _parser = new ExpressionParser();
 
-  public ExpressionParamHandler() {}
+    public ExpressionParamHandler() {}
 
-  public ExpressionParamHandler(ExpressionParamHandler handler, Param param) {
-    super(handler, param);
-  }
+    public ExpressionParamHandler(ExpressionParamHandler handler, Param param) {
+	super(handler, param);
+    }
 
   @Override
   public ParamHandler clone(Param paramToClone) {
-    return new ExpressionParamHandler(this, paramToClone);
+      return new ExpressionParamHandler(this, paramToClone);
   }
 
   @Override
   public String toInternalValue(RunnableObj<QueryInstanceSpec> ctxVals) throws WdkModelException {
+      String stableValue = ctxVals.get().get(_param.getName());
+      LOG.debug("transforming phyletic param: " + stableValue);
 
-    String stableValue = ctxVals.get().get(_param.getName());
-    LOG.debug("transforming phyletic param: " + stableValue);
+      // parse the expression and get the tree structure
+      ExpressionNode root = _parser.parse(stableValue);
+      String sql = composeSql(GROUP_SQL, root);
 
-    // parse the expression and get the tree structure
-    ExpressionNode root = _parser.parse(stableValue);
-
-    String sql = composeSql(GROUP_SQL, root);
-
-    return sql;
+      return sql;
   }
 
-  private String composeSql(String coreSql, ExpressionNode node) throws WdkModelException {
-    StringBuilder sql = new StringBuilder();
-    if (node instanceof BooleanNode) {
-      BooleanNode booleanNode = (BooleanNode) node;
-      sql.append(composeSql(coreSql, booleanNode.getLeft()));
-      if (booleanNode.getOperator().equals("and")) {
-        sql.append(" INTERSECT ");
-      }
-      else if (booleanNode.getOperator().equals("or")) {
-        sql.append(" UNION ");
-      }
-      sql.append(composeSql(coreSql, booleanNode.getRight()));
-    }
-    else {
-      LeafNode leaf = (LeafNode) node;
-      List<String> terms = leaf.getTerms();
-      StringBuilder sqlTerms = new StringBuilder();
-      for (String term : terms) {
-        String species = "'" + term + "'";
-        if (sqlTerms.length() > 1)
-          sqlTerms.append(",");
-        sqlTerms.append(species);
-      }
-      if (leaf.getCount() == 0 && leaf.getCondition().equals("=")) {
-        sql.append("((SELECT DISTINCT group_id FROM apidb.orthologgrouptaxon) MINUS ");
-      }
-      sql.append(coreSql);
-      sql.append(sqlTerms + ")");
-      sql.append(" GROUP BY group_id) WHERE ");
+    private String composeSql(String coreSql, ExpressionNode node) throws WdkModelException {
+	StringBuilder sql = new StringBuilder();
+	if (node instanceof BooleanNode) {
+	    BooleanNode booleanNode = (BooleanNode) node;
+	    sql.append(composeSql(coreSql, booleanNode.getLeft()));
+	    if (booleanNode.getOperator().equals("and")) {
+		sql.append(" INTERSECT ");
+	    }
+	    else if (booleanNode.getOperator().equals("or")) {
+		sql.append(" UNION ");
+	    }
+	    sql.append(composeSql(coreSql, booleanNode.getRight()));
+	}
+	else {
+	    LeafNode leaf = (LeafNode) node;
+	    List<String> terms = leaf.getTerms();
+	    StringBuilder sqlTerms = new StringBuilder();
+	    for (String term : terms) {
+		String species = "'" + term + "'";
+		if (sqlTerms.length() > 1)
+		    sqlTerms.append(",");
+		sqlTerms.append(species);
+	    }
 
-      if (leaf.isOnSpecies()) {
-        sql.append("number_of_taxa");
-      }
-      else {
-        sql.append("number_of_proteins");
-      }
+	    // PostgreSQL: Replace MINUS with EXCEPT
+	    if (leaf.getCount() == 0 && leaf.getCondition().equals("=")) {
+		sql.append("((SELECT DISTINCT group_id FROM apidb.orthologgrouptaxon) EXCEPT ");
+	    }
 
-      if (leaf.getCount() == 0 && leaf.getCondition().equals("=")) {
-        sql.append(">=1))");
-      }
-      else {
-        sql.append(leaf.getCondition() + leaf.getCount() + ")");
-      }
+	    sql.append(coreSql);
+	    sql.append(sqlTerms).append(")");
+	    sql.append(" GROUP BY group_id) WHERE ");
+
+	    if (leaf.isOnSpecies()) {
+		sql.append("number_of_taxa");
+	    }
+	    else {
+		sql.append("number_of_proteins");
+	    }
+
+	    if (leaf.getCount() == 0 && leaf.getCondition().equals("=")) {
+		sql.append(" >= 1))");
+	    }
+	    else {
+		sql.append(" ").append(leaf.getCondition()).append(leaf.getCount()).append(")");
+	    }
+	}
+	return sql.toString();
     }
-    return sql.toString();
-  }
 
 }
